af70454c698dc57b481a9859c839021119a2eda6
Leonel Zalegas <EliasZalegas@gmail.com>
2024-07-19 00:21:45 -0300

    -all the Parser related added -Repo implementation and Resources

--- app/src/main/java/com/example/stockmarketcheck/mainFeature/domain/repository/StockRepository.kt
+++ app/src/main/java/com/example/stockmarketcheck/mainFeature/domain/repository/StockRepository.kt
@@ -1,3 +1,12 @@
 package com.example.stockmarketcheck.mainFeature.domain.repository
 
-interface StockRepository
+import com.example.stockmarketcheck.mainFeature.domain.model.CompanyListing
+import com.example.stockmarketcheck.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+interface StockRepository {
+    suspend fun getCompanyListings(
+        fetchFromRemote: Boolean,
+        query: String,
+    ): Flow<Resource<List<CompanyListing>>>
+}
